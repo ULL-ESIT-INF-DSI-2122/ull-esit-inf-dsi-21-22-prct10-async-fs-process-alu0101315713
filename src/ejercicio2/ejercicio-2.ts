ed0cdc54ae8523415aed9aa4ec11093a622d6ff4
Felipe <alu0101315713@ull.edu.es>
2022-05-01 22:46:48 +0100

    ejercicio-2 con comentarios typedoc y sonarcloud

--- src/ejercicio2/ejercicio-2.ts
+++ src/ejercicio2/ejercicio-2.ts
@@ -1,10 +1,18 @@
 import {spawn} from 'child_process';
 import chalk from 'chalk';
-
+/**
+ * Reader class, it has 2 methods, 1 using pipe and
+ * another one without it
+ */
 export class Reader {
+  /**
+   * @param file file to read
+   */
   constructor(private file: string) {
   }
-
+  /**
+   * it makes a cat and grep command using pipe
+   */
   metodo1() {
     const cat = spawn('cat', ['-n', this.file]);
     const grep = spawn('grep', ['Hola']);
@@ -19,7 +27,9 @@ export class Reader {
       console.log(auxiliaryGrep);
     });
   }
-
+  /**
+ * it makes a cat and grep command without pipe
+ */
   metodo2() {
     const catGrep = spawn('cat', [this.file, 'grep', 'Hola']);
     let contador = 0;
